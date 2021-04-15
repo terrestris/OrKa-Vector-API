8a8bbb0f709f695640386154af0b1eb1e2aefe2b
jansule <suleiman@terrestris.de>
2021-04-15 10:39:00 +0200

    add routes, use psycopg

--- README.md
+++ README.md
@@ -5,30 +5,15 @@ The OrKa Vector REST API
 
 ## run dev server
 
-(optional) set path to non-default config file
-```shell
-export CONFIG_ABS_PATH=/path/to/config/file/config.py
-```
+TODO actually implement this
 
 start application
 ```shell
 FLASK_APP=orka_vector_api FLASK_ENV=development flask run
 ```
 
-## run prod server (waitress)
-
-TODO fix command/setup
+## run prod server from another venv (waitress)
 
 ```shell
-export CONFIG_ABS_PATH=/path/to/config/file/config.py
 waitress-serve --call 'orka_vector_api:create_app'
-```
-
-
-## db
-
-```shell
-FLASK_APP=orka_vector_api FLASK_ENV=development flask db init
-FLASK_APP=orka_vector_api FLASK_ENV=development flask db migrate
-FLASK_APP=orka_vector_api FLASK_ENV=development flask db upgrade
 ```
--- orka_vector_api/views/data.py
+++ orka_vector_api/views/data.py
@@ -1,3 +1,31 @@
-from flask import Blueprint
+import json
+
+from flask import Blueprint, send_file, current_app, abort
+
+from orka_vector_api import db
+from orka_vector_api.helper import get_job_by_id, get_job_id_by_dataid
 
 data = Blueprint('data', __name__, url_prefix='/data')
+
+
+@data.route('/<uuid:data_id>', methods=['GET'])
+def get_data(data_id):
+    # TODO get absolute path to folder where gpkgs are stored
+    # TODO provide downloaded gpkg
+    data_id_str = str(data_id)
+    filename = data_id_str + '.gpkg'
+    gpkg_abs_path = ''
+
+    conn = db.pool.getconn()
+
+    job_id = get_job_id_by_dataid(data_id_str, conn, current_app)
+    if job_id is None:
+        abort(404)
+
+    job = get_job_by_id(job_id, conn, current_app)
+
+    db.pool.putconn(conn)
+    # TODO check if file exists, if yes, then provide it, otherwise 404
+
+    # return send_file(gpkg_abs_path, attachment_filename=filename)
+    return json.dumps({'success': True})
